19ae432df1b0b0d81c7ac7bbde173b853c20809d
Yummyto <80955467+Yummyto@users.noreply.github.com>
2025-07-23 23:56:11 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <HashRouter basename="/LLCCProject">
+        <HashRouter>
             <App />
         </HashRouter>
     </React.StrictMode>
